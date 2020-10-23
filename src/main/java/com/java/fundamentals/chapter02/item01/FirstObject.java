ae7b4016356448ca12a3134401970e5d78254a2b
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-10-23 02:12:37 +0100

    add examples

---
+++ src/main/java/com/java/fundamentals/chapter02/item01/FirstObject.java
@@ -0,0 +1,14 @@
+package com.java.fundamentals.chapter02.item01;
+
+public class FirstObject {
+
+    private final String value;
+    /**
+     * Here you are forced to only pass a value on this constructor.
+     * However, you don't have the flexibility to state the intent
+     * of what the value will do within this constructor.
+     * */
+    public FirstObject(String value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/com/java/fundamentals/chapter02/item01/SecondObject.java
@@ -0,0 +1,18 @@
+package com.java.fundamentals.chapter02.item01;
+
+public class SecondObject {
+    private final String value;
+
+    private SecondObject(String value) {
+        this.value = value;
+    }
+
+    /**
+     * Here you have the flexibility of naming your static method to show
+     * intent of what you are doing. In this case, here we are creating
+     * a SecondObject from a String value.
+     * */
+    public static SecondObject from(String value) {
+        return new SecondObject(value);
+    }
+}
